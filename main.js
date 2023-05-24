924bfd4c2d7722ede1c559cf2fb94cd1e04e5ab7
jamesaking02 <jamesaking02@outlook.com>
2023-05-23 21:10:38 -0700

    finished programming of menu

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 const menuButton = document.querySelector(".menu-button")
 const closeButton = document.querySelector(".close-button")
 const body = document.querySelector("body")
-const nav = document.querySelector("nav")
+const overlay = document.querySelector(".overlay")
+const nav = document.querySelector(".nav-absolute")
 let menuOpen = false
-console.log(nav)
 
-function menuControl() {
-
+function openMenu() {
+  overlay.classList.remove("closed")
+  nav.style.transform = "translateX(0%)"
+  nav.style.transition = "all .2s ease-in"
+  menuButton.classList.add("opened")
+  closeButton.style.transform = "translateY(0)"
+  closeButton.style.transition = "all .1s ease-in-out"
+  body.classList.add("fixed")
 }
+
+function closeMenu() {
+  overlay.classList.add("closed")
+  nav.style.transform = "translateX(100%)"
+  menuButton.classList.remove("opened")
+  closeButton.style.transform = "translateY(-10vh)"
+  body.classList.remove("fixed")
+}
+
+menuButton.addEventListener("click", openMenu)
+closeButton.addEventListener("click", closeMenu)
