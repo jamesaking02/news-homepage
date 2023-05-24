a8277a936cec3a7a0f56bfbbf13c6b93905fa7f4
jamesaking02 <jamesaking02@outlook.com>
2023-05-23 20:07:07 -0700

    declared variables

---
+++ main.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector(".menu-button")
+const closeButton = document.querySelector(".close-button")
+const body = document.querySelector("body")
+const nav = document.querySelector("nav")
+let menuOpen = false
+console.log(nav)
+
+function menuControl() {
+
+}
